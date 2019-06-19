6088f734b87c1f6a3ba8737b5f49d11d8fb5daf8
madDariya <dariya.ashchepkova@gmail.com>
2019-06-19 11:19:42 +0300

    Ready to run on heroku

--- bot.js
+++ bot.js
@@ -13,11 +13,55 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.on('message', (msg) => {
-  const name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
-    // reply sent!
+bot.on(/\/get/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'What currency do you want to check?',{
+    reply_markup: {
+        inline_keyboard: [
+          [
+              {
+                  text: '€ - EUR',
+                  callback_data: 'EUR'
+              },
+              {
+                  text: '$ - USD',
+                  callback_data: 'USD'
+              },
+              {
+                  text: '₽ - RUR',
+                  callback_data: 'RUR'
+              },
+              {
+                  text: '₿ - BTC',
+                  callback_data: 'BTC'
+              }
+          ]
+        ]
+    }
   });
 });
 
+bot.on('callback_query', query => {
+    //console.log(query);
+    const id = query.message.chat.id;
+
+    request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function(error, response, body){
+        const data = JSON.parse(body);
+        const result = data.filter(item => item.ccy === query.data)[0];
+        const flag = {
+            'EUR': '🇪🇺',
+            'USD': '🇺🇸',
+            'RUR': '🇷🇺',
+            'UAH': '🇺🇦',
+            'BTC': '₿'
+          }
+        let md = `
+        *${flag[result.ccy]} ${result.ccy} 💱 ${result.base_ccy} ${flag[result.base_ccy]}*
+      Buy: _${result.buy}_
+      Sale: _${result.sale}_
+        `;
+        bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+    })
+})
 module.exports = bot;
